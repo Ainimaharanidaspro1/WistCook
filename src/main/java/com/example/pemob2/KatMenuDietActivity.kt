8c2a00ad81978e8db25965c0debbf69b81a74b67
JessicaAlmalia <128279005+JessicaAlmalia@users.noreply.github.com>
2024-11-20 10:39:08 +0700

    Add files via upload

---
+++ src/main/java/com/example/pemob2/KatMenuDietActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kategorimenudiet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KatMenuDietActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kat_menudiet)
+    }
+}
---
+++ src/main/java/com/example/pemob2/KatPuddingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kategoripudding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KatPuddingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kat_pudding)
+    }
+}
